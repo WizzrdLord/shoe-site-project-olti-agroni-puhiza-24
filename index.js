828e2b1111b44690d23072e4870af910ab808d5a
Puhiza <kurtishajpuhiza@gmail.com>
2024-11-26 12:29:31 +0100

    javascript per item switch

---
+++ index.js
@@ -0,0 +1,14 @@
+const slides = document.getElementsByClassName('new-container');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active')
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active')
+}
