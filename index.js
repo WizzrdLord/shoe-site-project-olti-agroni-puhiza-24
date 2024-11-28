973cfc4835b163fe0c023c4c8ab18dbdc13758c9
Smoke <150609260+SMKSmokez@users.noreply.github.com>
2024-11-28 19:35:01 +0100

    Added Gallery
    Added html and css for the gallery section of the page, tweaked the js and @media

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+/*Landing Page Image Select*/
 const slides = document.getElementsByClassName('new-container');
-let index = 0;
+let l_index = 0;
 
 function next(){
-    slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active')
+    slides[l_index].classList.remove('active');
+    l_index = (l_index + 1) % slides.length;
+    slides[l_index].classList.add('active')
 }
 
 function prev(){
-    slides[index].classList.remove('active');
-    index = (index - 1 + slides.length) % slides.length;
-    slides[index].classList.add('active')
+    slides[l_index].classList.remove('active');
+    l_index = (l_index - 1 + slides.length) % slides.length;
+    slides[l_index].classList.add('active')
 }
