2d25696840a9fef998c1a62b9439ea35b41f63e3
dushkaja-1 <87616927+WizzrdLord@users.noreply.github.com>
2024-12-22 22:34:58 +0100

    Forgot to push :P

---
+++ slashOp/blogsOp.php
@@ -0,0 +1,9 @@
+
+    <p>Shëno Titullin e Bllogut</p>
+    <input id="blogs_new_title">Titulli Shkon Këtu</input>
+    
+    <p>Shëno Bllogun</p>
+    <input id="blogs_new_content">Bllogu Shkon Këtu</input>
+    
+    <p>Shëno Titullin e Bllogut</p>
+    <input id="blogs_new_date" type="date"></input>
