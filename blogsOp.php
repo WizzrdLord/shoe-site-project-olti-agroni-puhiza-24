6895008b66e6fde87dd89c17ab0860612e5dd51d
dushkaja-1 <87616927+WizzrdLord@users.noreply.github.com>
2025-01-10 20:39:32 +0100

    imessed up

--- blogsOp.php
+++ blogsOp.php
@@ -1,13 +1,50 @@
-<form method="POST" action="">
-    <input type="hidden" name="action" value="add_blog">
+<form class="blog-form" method="POST" action="">
     <p>Shëno Titullin e Bllogut</p>
-    <input id="blogs_new_title" name="blog_title" placeholder="Titulli Shkon Këtu" required>
+    <input class="blog-input" id="blogs_new_title" name="blog_title" placeholder="Titulli Shkon Këtu" required>
     
     <p>Shëno Bllogun</p>
-    <textarea id="blogs_new_content" name="blog_content" placeholder="Bllogu Shkon Këtu" required></textarea>
+    <textarea class="blog-textarea" id="blogs_new_content" name="blog_content" placeholder="Bllogu Shkon Këtu" required></textarea>
     
     <p>Shëno Ditën e Publikimit të Bllogut</p>
-    <input id="blogs_new_date" name="blog_creation_date" type="date" required>
+    <input class="blog-date" id="blogs_new_date" name="blog_creation_date" type="date" required>
 
-    <button type="submit" id="submit">Shto Artikullin</button>
+    <button class="blog-button" type="submit" id="submit">Shto Artikullin</button>
+
+    <input class="blog-input" list="delArticle" placeholder="Zgjidh artikullin per ta fshire">
+
+    <button class="blog-button" type="submit" id="submit">Fshij Artikullin</button>
+
+    <style>
+        .blog-form{
+            padding:20px;
+            border-color: gray;
+            border-radius: 10px;
+            background-color: lightsalmon;
+            margin-top: 50px;
+            margin-left: 50px;
+            align-items: center;
+        }
+        .blog-input{
+            border-color: black;
+            border-radius: 2px;
+            height: 20px;
+        }
+        .blog-textarea{
+            border-color: black;
+            border-radius: 2px;
+            padding: 10px;
+            height: 400px;
+            width: 200px;
+        }
+        .blog-button{
+            border-radius: 2px;
+            padding: 10px;
+            background-color: gray;
+            color:white;
+            padding: 10px;
+        }
+        .blog-date{
+            padding: 10px;
+        }
+    </style>
 </form>
