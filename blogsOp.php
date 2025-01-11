7676faa8bb440a3367fdea6aa3e82e6718a9488c
dushkaja-1 <87616927+WizzrdLord@users.noreply.github.com>
2025-01-11 17:27:15 +0100

    Made a delete article functionality (I think)

--- blogsOp.php
+++ blogsOp.php
@@ -1,50 +1,32 @@
 <form class="blog-form" method="POST" action="">
     <p>Shëno Titullin e Bllogut</p>
     <input class="blog-input" id="blogs_new_title" name="blog_title" placeholder="Titulli Shkon Këtu" required>
-    
+
     <p>Shëno Bllogun</p>
     <textarea class="blog-textarea" id="blogs_new_content" name="blog_content" placeholder="Bllogu Shkon Këtu" required></textarea>
-    
+
     <p>Shëno Ditën e Publikimit të Bllogut</p>
     <input class="blog-date" id="blogs_new_date" name="blog_creation_date" type="date" required>
 
     <button class="blog-button" type="submit" id="submit">Shto Artikullin</button>
+</form>
 
-    <input class="blog-input" list="delArticle" placeholder="Zgjidh artikullin per ta fshire">
+<form class="blog-form" method="POST" action="">
+    <p>Zgjidhni artikullin te cilin doni te fshini.</p>
+    <br>
+    <select name="blog_name" id="blog_name" required>
+        <?php
+        $sql = "SELECT blog_title FROM `blogs-table`";
+        $result = $conn->query($sql);
 
-    <button class="blog-button" type="submit" id="submit">Fshij Artikullin</button>
-
-    <style>
-        .blog-form{
-            padding:20px;
-            border-color: gray;
-            border-radius: 10px;
-            background-color: lightsalmon;
-            margin-top: 50px;
-            margin-left: 50px;
-            align-items: center;
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<option value=\"" . htmlspecialchars($row['blog_title']) . "\">" . htmlspecialchars($row['blog_title']) . "</option>";
+            }
+        } else {
+            echo "<option value=\"\">No blogs available</option>";
         }
-        .blog-input{
-            border-color: black;
-            border-radius: 2px;
-            height: 20px;
-        }
-        .blog-textarea{
-            border-color: black;
-            border-radius: 2px;
-            padding: 10px;
-            height: 400px;
-            width: 200px;
-        }
-        .blog-button{
-            border-radius: 2px;
-            padding: 10px;
-            background-color: gray;
-            color:white;
-            padding: 10px;
-        }
-        .blog-date{
-            padding: 10px;
-        }
-    </style>
+        ?>
+        <button type="submit" class="delete-button">Delete Blog</button>
+    </select>
 </form>
